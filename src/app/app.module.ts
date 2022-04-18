273d67cae46d4c96ca2d0816ae47157f5cc69fb4
prasannakarki <36865903+prasannakarki@users.noreply.github.com>
2022-04-18 17:59:36 -0500

    Added Bootstrap,added footer and header/menu component hardcoded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { ErrorComponent } from './error/error.component';
 import { ListTodosComponent } from './list-todos/list-todos.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +18,8 @@ import { ListTodosComponent } from './list-todos/list-todos.component';
     LoginComponent,
     ErrorComponent,
     ListTodosComponent,
+    MenuComponent,
+    FooterComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],

6b2926eb6e2535ad1d0cbfaa07b854cead5017c5
prasannakarki <36865903+prasannakarki@users.noreply.github.com>
2022-04-18 23:06:38 -0500

    session storage

--- src/app/service/hardcoded-authentication.service.ts
+++ src/app/service/hardcoded-authentication.service.ts
@@ -7,10 +7,18 @@ export class HardcodedAuthenticationService {
   constructor() {}
 
   authenticate(username: string, password: string) {
+    console.log('before ' + this.isUserLoggedIn());
     if (username === 'prasanna' && password === 'karki') {
-      //redirect to welcome page if password matches
+      //stores in browser session. username.
+      sessionStorage.setItem('authenticateUser', username);
+      console.log('after ' + this.isUserLoggedIn());
+
       return true;
     }
     return false;
+  }
+  isUserLoggedIn() {
+    let user = sessionStorage.getItem('authenticateUser');
+    return !(user === null);
   }
 }

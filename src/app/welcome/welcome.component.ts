299e2e64675154ca12579c004a74872d8d661835
prasannakarki <36865903+prasannakarki@users.noreply.github.com>
2022-04-19 23:47:23 -0500

    connection to backend

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AppComponent } from '../app.component';
+import { WelcomeDataService } from '../service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -12,13 +13,21 @@ export class WelcomeComponent implements OnInit {
   name = '';
 
   //Activated Route to accept parameters in url.
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private service: WelcomeDataService
+  ) {}
 
   ngOnInit() {
     console.log(this.message);
     //prints a name param in console which it grabs from Activated Router.
     console.log(this.route.snapshot.params['name']);
     this.name = this.route.snapshot.params['name'];
+  }
+  getWelcomeMessage() {
+    console.log(this.service.executeHelloWorldBeanService());
+    this.service.executeHelloWorldBeanService().subscribe();
+    //console.log('Get Welcome Message.');
   }
 }
 

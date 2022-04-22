07a42e2d58836a0df49b803d9e51fbf4d79e0bd2
prasannakarki <36865903+prasannakarki@users.noreply.github.com>
2022-04-21 22:59:22 -0500

    getting todos from backend hardcoded at the moment

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoDataService } from '../service/data/todo-data.service';
 
 export class Todo {
   constructor(
@@ -14,16 +15,22 @@ export class Todo {
   styleUrls: ['./list-todos.component.css'],
 })
 export class ListTodosComponent implements OnInit {
-  todos = [
-    new Todo(1, 'Learn to code', false, new Date()),
-    new Todo(2, 'Learn SpringBoot', false, new Date()),
-    new Todo(3, 'Learn Angular', false, new Date()),
-  ];
+  todos: Todo[];
+  // todos = [
+  //   new Todo(1, 'Learn to code', false, new Date()),
+  //   new Todo(2, 'Learn SpringBoot', false, new Date()),
+  //   new Todo(3, 'Learn Angular', false, new Date()),
+  // ];
   // todo = {
   //   id: 1,
   //   description: 'Learn to Code',
   // };
-  constructor() {}
+  constructor(private todoService: TodoDataService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.todoService.retrieveAllTodos('Prasanna').subscribe((response) => {
+      console.log(response);
+      this.todos = response;
+    });
+  }
 }

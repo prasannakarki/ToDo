4c8c1571ab2f27da5f6524af088a90422fd7a9fa
prasannakarki <36865903+prasannakarki@users.noreply.github.com>
2022-05-10 21:45:15 -0500

    update module created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ListTodosComponent } from './list-todos/list-todos.component';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
 import { RouteGuardService } from './service/route-guard.service';
+import { TodoComponent } from './todo/todo.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
@@ -24,6 +25,11 @@ const routes: Routes = [
   {
     path: 'logout',
     component: LogoutComponent,
+    canActivate: [RouteGuardService],
+  },
+  {
+    path: 'update',
+    component: TodoComponent,
     canActivate: [RouteGuardService],
   },
   { path: '**', component: ErrorComponent },
